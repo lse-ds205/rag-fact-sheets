fbda2c69673062569f1454b8561dc7dca64e52f7
Ariel Liu <arielliu@Ariels-MacBook-Pro.local>
2025-05-20 22:34:42 +0100

    cleaned up a bit

--- create_table.sql
+++ create_table.sql
@@ -5,11 +5,15 @@
 
 CREATE EXTENSION IF NOT EXISTS vector;
 
-DROP TABLE IF EXISTS document_embeddings;
+-- This line drops the table if it already exists, be careful if there's data already
+-- Only use it when the table is not created or there is some other error:
+-- DROP TABLE IF EXISTS document_embeddings; (unhighlight this line to use it)
 
 CREATE TABLE document_embeddings (
     id SERIAL PRIMARY KEY,
     document_id TEXT,
     country_code TEXT,
+    original_text TEXT,
+    source_hyperlink TEXT,
     embedding VECTOR(768)
 );
